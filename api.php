7dd2022f92ed6eb5a4f306a6d46b2f0c272bc201
Tilican <Tilican@users.noreply.github.com>
2024-10-24 07:48:49 +0000

    deploy: a12f53331e9fcc964f34317d8f19533523f7364c

--- api.php
+++ api.php
@@ -3,18 +3,46 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *'); // for dev purpose
 
+$c = $_GET['c'] ?? null;
+$m = $_GET['m'] ?? null;
+
 try {
     $pdo = new PDO('sqlite:/home/smitefrawc/db-vgs/vgs.db');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $pdo->prepare("SELECT name FROM gods") ;
-    $stmt->execute();
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    switch ($c) {
+        case 'gods':
+            $stmt = $pdo->prepare("SELECT name FROM gods") ;
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if ($results) {
-        echo json_encode($results);
-    } else {
-        echo json_encode([]);
+            if ($results) {
+                echo json_encode($results);
+            } else {
+                echo json_encode([]);
+            }
+            exit;
+
+        case 'skins':
+            http_response_code(404);
+            echo json_encode([
+                'error' => 'TODO'
+            ]);
+            exit;
+
+        case 'vgs':
+            http_response_code(404);
+            echo json_encode([
+                'error' => 'TODO'
+            ]);
+            exit;
+
+        default:
+            http_response_code(404);
+            echo json_encode([
+                'error' => '"c" params required.'
+            ]);
+            exit;
     }
 
 } catch (PDOException $e) {
