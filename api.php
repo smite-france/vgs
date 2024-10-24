d0e61ac5f9d655c66c67846976173d2101a3af9f
Tilican <Tilican@users.noreply.github.com>
2024-10-24 07:42:11 +0000

    deploy: e2720a8f6f640e76578338616cc9127e980089ad

--- api.php
+++ api.php
@@ -1,28 +1,22 @@
 <?php
-try {
-    // Connect to SQLite database
-    $pdo = new PDO('sqlite:/home/smitefrawc/db-vgs/vgs.db');
 
-    // Set error mode to exceptions
+header('Content-Type: application/json');
+
+try {
+    $pdo = new PDO('sqlite:/home/smitefrawc/db-vgs/vgs.db');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // SQL query to select data from the table
-    $sql = "SELECT name FROM gods";
-
-    // Prepare and execute the SQL query
-    $stmt = $pdo->prepare($sql);
+    $stmt = $pdo->prepare("SELECT name FROM gods");
     $stmt->execute();
-
-    // Fetch all the results
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Display the results
-    foreach ($results as $row) {
-        print_r($row);
+    if ($results) {
+        echo json_encode($results);
+    } else {
+        echo json_encode([]);
     }
 
 } catch (PDOException $e) {
-    // Handle any errors
     echo 'Connection failed: ' . $e->getMessage();
 }
 
