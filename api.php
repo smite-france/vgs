a991699d8cbebc9dd0d92816cdebde9a11b7c93d
Tilican <Tilican@users.noreply.github.com>
2024-10-28 16:50:16 +0000

    deploy: e71a2c6e97f086baa6d726a6b19942b5e0f33df1

--- api.php
+++ api.php
@@ -74,7 +74,13 @@ try {
                         exit;
                     }
 
-                    $stmt = $pdo->prepare("SELECT DISTINCT name FROM vgs WHERE id_skin = :id_skin;");
+                    $stmt = $pdo->prepare("SELECT DISTINCT vgs.name
+FROM vgs
+         JOIN skins ON vgs.id_skin = skins.id
+         JOIN gods ON skins.id_god = gods.id
+WHERE vgs.id_skin = :id_skin
+   OR (LOWER(gods.name) = 'default' AND LOWER(skins.name) = 'default')
+   OR (skins.id = :id_skin AND LOWER(skins.name) LIKE '%standard%');");
                     $stmt->bindParam(':id_skin', $id_skin);
                     $stmt->execute();
                     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
